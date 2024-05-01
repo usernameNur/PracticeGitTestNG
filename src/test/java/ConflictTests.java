6bffa869b4d6eff0055a2f61292a42ec2703905c
Nurzada Abdullaeva <ab.nurzada@Nurzadas-MacBook-Air.local>
2024-05-01 16:36:20 -0500

    pushed

--- src/test/java/ConflictTests.java
+++ src/test/java/ConflictTests.java
@@ -1,8 +1,19 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ConflictTests {
     @Test
-    public void test(){
-        System.out.println("creating test");
+    public void test1(){
+        int a=5;
+        int b=10;
+        int sum= (a+b);
+        System.out.println("sum: "+(a+b));
+
+    }
+    @Test
+    public void test2(){
+        String message="Welcome to Usa, new oportunities";
+        Assert.assertTrue(message.contains("Usa"));
+        //src/test/java/ConflictTests.java
     }
 }
