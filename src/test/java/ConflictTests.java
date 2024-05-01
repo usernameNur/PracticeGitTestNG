c085389c5ec5634a636f911e98fceae6239efdce
Nurzada Abdullaeva <ab.nurzada@Nurzadas-MacBook-Air.local>
2024-05-01 16:18:34 -0500

    message

--- src/test/java/ConflictTests.java
+++ src/test/java/ConflictTests.java
@@ -11,9 +11,8 @@ public class ConflictTests {
     }
     @Test
     public void test2(){
-        String message="Welcome to Usa";
+        String message="Welcome to Usa, new oportunities";
         Assert.assertTrue(message.contains("Usa"));
-
-
+        //src/test/java/ConflictTests.java
     }
 }
